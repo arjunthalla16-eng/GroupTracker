e7c5cdf8f2af6d535fc0fda8ccbbc9619f1195b9
arjunthalla16-eng <arjunthalla16@gmail.com>
2026-08-15 19:46:53 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,9 @@
-console.log("GroupTracker JavaScript loaded!");
+document.getElementById("createBtn").addEventListener("click", function () {
+    document.getElementById("homePage").classList.add("hidden");
+    document.getElementById("createPage").classList.remove("hidden");
+});
+
+document.getElementById("joinBtn").addEventListener("click", function () {
+    document.getElementById("homePage").classList.add("hidden");
+    document.getElementById("joinPage").classList.remove("hidden");
+});
