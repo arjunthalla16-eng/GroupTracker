7d0c082b8c3b4645a8266eb188f673ec8f16ea51
arjunthalla16-eng <arjunthalla16@gmail.com>
2026-08-15 19:56:22 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,3 @@
-// ================================
-// GET ELEMENTS
-// ================================
-
 const homePage = document.getElementById("homePage");
 const createPage = document.getElementById("createPage");
 const joinPage = document.getElementById("joinPage");
@@ -13,53 +9,41 @@ const backBtn = document.getElementById("backBtn");
 const joinBackBtn = document.getElementById("joinBackBtn");
 
 
-// ================================
 // CREATE EVENT
-// ================================
 
 createBtn.addEventListener("click", function () {
 
     homePage.classList.add("hidden");
-
     createPage.classList.remove("hidden");
 
 });
 
 
-// ================================
 // JOIN EVENT
-// ================================
 
 joinBtn.addEventListener("click", function () {
 
     homePage.classList.add("hidden");
-
     joinPage.classList.remove("hidden");
 
 });
 
 
-// ================================
 // BACK FROM CREATE
-// ================================
 
 backBtn.addEventListener("click", function () {
 
     createPage.classList.add("hidden");
-
     homePage.classList.remove("hidden");
 
 });
 
 
-// ================================
 // BACK FROM JOIN
-// ================================
 
 joinBackBtn.addEventListener("click", function () {
 
     joinPage.classList.add("hidden");
-
     homePage.classList.remove("hidden");
 
 });
